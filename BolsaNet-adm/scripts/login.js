421b4f4abfcc2358b287a333200fd3be0660fb3d
YestXx <lucasbarbosaareado34@gmail.com>
2025-10-01 10:44:27 -0300

    inicio da estrutura de administração

---
+++ BolsaNet-adm/scripts/login.js
@@ -0,0 +1,28 @@
+document.querySelector('form').addEventListener('submit', async function (e) {
+    e.preventDefault(); // Impede o envio padrão do formulário
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('http://localhost:3001/user/login', {
+            method: 'POST',
+            headers: {
+
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password }) 
+        });
+
+        const result = await response.json();
+
+        if (response.ok && result.token) {
+            window.location.href = 'home.html';
+        } else {
+            alert(result.error || 'Erro ao fazer login.');
+        }
+    } catch (error) {
+        console.error('Erro na requisição:', error);
+        alert('Erro de conexão com o servidor.');
+    }
+});
